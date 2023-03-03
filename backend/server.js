3bbbcf54367fddc46ac871a918f1e0cbeccd7172
Faiz <faizkfz@gmail.com>
2023-03-03 19:02:52 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -24,14 +24,7 @@ connectDb();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended:false }));
-app.use(cors(
-    {
-        origin: ["http://localhost:3000",
-                "https://taskManagerApp-api.onrender.com"
-    
-    ]
-    }
-));
+app.use(cors());
 // backend uri  : https://taskManagerApp-api.onrender.com
 app.use("/api/tasks" ,taskRoutes);
 
